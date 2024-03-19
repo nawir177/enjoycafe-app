c5eb3d5d9f46f5b98564d1417eee527c5f015cbe
muhammad  Nawir <m.nawir177@gmail.com>
2024-03-19 23:34:19 +0800

    delete banner

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,15 +7,15 @@ import App from "../App";
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 const apiUrl = process.env.REACT_APP_API_URL;
 export default function Home() {
-   const url = apiUrl + '/home'
-   const { data, error, isLoading } = useSWR(url, fetcher)
-   if (error) return <div>failed to load</div>
-   if (isLoading) return <div>loading...</div>
+   // const url = apiUrl + '/home'
+   // const { data, error, isLoading } = useSWR(url, fetcher)
+   // if (error) return <div>failed to load</div>
+   // if (isLoading) return <div>loading...</div>
    return (
       <main>
          <App title={'home'}>
             <div className="mb-6">
-               <Banner carosel={data.data.sliders} />
+               {/* <Banner carosel={data.data.sliders} /> */}
             </div>
             <div className="mb-6">
                <Category />
